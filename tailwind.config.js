7e9a0e82de79b22e74fd6020272fc685d593a032
vipulpathak113 <vipulpathak113@gmail.com>
2023-10-24 19:34:46 +0530

    config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#FF6363',
+      }
+    },
+  },
+  plugins: [],
+}
+
